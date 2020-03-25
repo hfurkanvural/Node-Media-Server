7d44cf72b95d28766848be798e89e86fea802a40
hfurkanvural <hfurkanvural@gmail.com>
2020-03-25 21:44:29 +0300

    first api added and nodemon initialized

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const express = require("express");
 const path = require("path");
 const fs = require('fs')
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello from Node API!");
+  });
+
+
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
