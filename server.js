7fcaae72167c6fe57866add999ccd1cb1e8b2c3e
hfurkanvural <hfurkanvural@gmail.com>
2020-03-25 22:10:44 +0300

    get data service added.

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const path = require("path");
+const fs = require('fs')
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'))
+  });
+
+app.get('/data/:id', function(req, res) {
+  const path = 'data/'+req.params.id;
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const range = req.headers.range
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1]
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+
+    if(start >= fileSize) {
+      res.status(416).send('Requested range not satisfiable\n'+start+' >= '+fileSize);
+      return
+    }
+    
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    }
+
+    res.writeHead(206, head)
+    file.pipe(res)
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});    
